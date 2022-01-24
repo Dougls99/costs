0206ad6d810fb524084ffa61f8edd62341a24e75
Douglas Pereira <dougls99@outlook.com>
2022-01-24 09:11:10 -0300

    Finalizando Pag de Edição de Projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,12 +3,22 @@ import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+import Message from '../layout/Message'
+import ProjectForm from '../project/ProjectForm'
+
 function Project() {
   const {id} = useParams()
+
   const [project, setProject] = useState([])
+  const [showProjectForm, setShowProjectForm] = useState(false)
+  const [message, setMessage] = useState()
+  const [type, setType] = useState()
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -19,10 +29,73 @@ function Project() {
       setProject(data)
     })
     .catch((err) => console.log)
+    }, 2000)
   }, [id])
 
+  function editPost(project) {
+    // budget validation
+    if(project.budget < project.cost) {
+      setMessage('O Orçamento Não Pode Ser Menor Que o Custo do Projeto!')
+      setType('error')
+      return false
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+    .then(resp => resp.json())
+    .then((data) => {
+
+      setProject(data)
+      setShowProjectForm(false)
+      setMessage('Projeto Atualizado!')
+      setType('success')
+    })
+    .catch(err => console.log(err))
+  }
+
+  function toggleProjectForm() {
+    setShowProjectForm (!showProjectForm)
+  }
+
   return (
-    <p>{project.name}</p>
+    <>{project.name ? (
+      <div className={styles.project_details}>
+        <Container customClass="column">
+          {message && <Message type={type} msg={message} />}
+          <div className={styles.details_container}>
+            <h1>Projeto: {project.name}</h1>
+            <button className={styles.btn} onClick={toggleProjectForm}>
+            {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+            </button>
+            {!showProjectForm ? (
+              <div className={styles.project_info}> 
+                <p>
+                 <span>Categoria:</span> {project.category.name}
+                </p>
+                <p>
+                 <span>Total de Orçamento:</span> R${project.budget}
+                </p>
+                <p>
+                 <span>Total de Utilizado:</span> R${project.cost}
+                </p>
+              </div>
+            ) : (
+              <div className={styles.project_info}>
+                <ProjectForm 
+                handleSubmit={editPost} 
+                btnText="Concluir Edição" 
+                projectData={project} />
+              </div>
+            )}
+          </div>
+        </Container>
+      </div> )
+      : <Loading />}</>
   )
 }
 
