2942f6ced4d24b40ddfc02fcba2802962f6ea783
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-29 19:24:08 -0300

    import's ajustado no pages/NewProject e identacao do prettier

--- src/pages/NewProject/index.js
+++ src/pages/NewProject/index.js
@@ -1,33 +1,31 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-import ProjectForm from '../project/ProjectForm';
-
-import styles from './NewProject.module.css';
+import { ProjectForm } from "../../components";
+import styles from "./NewProject.module.css";
 
 function NewProject() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function createPost(project) {
-
     //initialize cost and services
-    project.cost = 0
-    project.services = []
+    project.cost = 0;
+    project.services = [];
 
-    fetch('http://localhost:5000/projects', {
-      method: 'POST',
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(project)
-    }) 
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data) 
-      navigate('/projects', {state: { message: 'Projeto criado com sucesso!' }})
+      body: JSON.stringify(project),
     })
-    .catch(err => console.log(err))
-
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        navigate("/projects", {
+          state: { message: "Projeto criado com sucesso!" },
+        });
+      })
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -36,7 +34,7 @@ function NewProject() {
       <p>Crie seu projeto para depois adicionar os serviços</p>
       <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
-  )
+  );
 }
 
-export default NewProject
+export default NewProject;
