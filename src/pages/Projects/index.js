4d86a962e4b003cfdbdd849dbd61a69cad076626
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-29 19:18:36 -0300

    import's ajustado no pages/Projects e identacao do prettier

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,53 +1,57 @@
-import { useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import Message from '../layout/Message'
-import Container  from '../layout/Container'
-import LinkButton from '../layout/LinkButton'
-import ProjectCard from '../project/ProjectCard'
-import Loading from '../layout/Loading'
-
-import styles from './Projects.module.css'
+import {
+  Container,
+  LinkButton,
+  Loading,
+  Message,
+  ProjectCard,
+} from "../../components";
+import styles from "./Projects.module.css";
 
 function Projects() {
-  const [projects, setProjects] = useState ([])
-  const [removeLoading, setRemoveLoading] = useState(false)
-  const [projectMessage, setProjectMessage] = useState('')
+  const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
+  const [projectMessage, setProjectMessage] = useState("");
 
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
   if (location.state) {
-    message = location.state.message
+    message = location.state.message;
   }
 
   useEffect(() => {
     //Para visualizar Loading
-    setTimeout(() => 
-      fetch ('http://localhost:5000/projects', {
-      method: 'GET',
-      headers:{
-        'Content-type': 'application/json',
-      },
-    })
-    .then(resp => resp.json())
-    .then((data) => {
-      setProjects (data)
-      setRemoveLoading(true)
-    }),
-     100,)
-  }, [])
+    setTimeout(
+      () =>
+        fetch("http://localhost:5000/projects", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProjects(data);
+            setRemoveLoading(true);
+          }),
+      100
+    );
+  }, []);
 
   function removeProject(id) {
     fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-    }).then(resp => resp.json())
-    .then((data) => {
-      setProjects(projects.filter((project) => project.id !== id))
-      setProjectMessage('Projeto removido com sucesso!')
     })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(projects.filter((project) => project.id !== id));
+        setProjectMessage("Projeto removido com sucesso!");
+      });
   }
 
   return (
@@ -59,24 +63,24 @@ function Projects() {
       {message && <Message type="success" msg={message} />}
       {projectMessage && <Message type="success" msg={projectMessage} />}
       <Container customClass="start">
-        {projects.length > 0 && 
-         projects.map((project) => ( 
-         <ProjectCard
-          id={project.id}
-          name={project.name}
-          budget={project.budget}
-          category={project.category.name}
-          key={project.id}
-          handleRemove={removeProject}
-          />
-        ))}
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+              handleRemove={removeProject}
+            />
+          ))}
         {!removeLoading && <Loading />}
         {removeLoading && projects.length === 0 && (
           <p>Não há projetos cadastrados!</p>
         )}
       </Container>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
