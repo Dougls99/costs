46a241acc6c4edb4ca556a115f81c93ebee562cc
Douglas Pereira <dougls99@outlook.com>
2022-02-02 11:14:50 -0300

    Melhorias no projeto

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Container from "./Container";
+import Footer from "./Footer";
+import Input from "./Input";
+import LinkButton from "./LinkButton";
+import Loading from "./Loading";
+import Message from "./Message";
+import Navbar from "./Navbar";
+import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
+import Select from "./Select";
+import ServiceCard from "./ServiceCard";
+import ServiceForm from "./ServiceForm";
+import SubmitButton from "./SubmitButton";
+
+export {
+  Container,
+  Footer,
+  Input,
+  LinkButton,
+  Loading,
+  Message,
+  Navbar,
+  ProjectCard,
+  ProjectForm,
+  Select,
+  ServiceCard,
+  ServiceForm,
+  SubmitButton,
+};
